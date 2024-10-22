a31599c1dbd87b75312623e234fe97ee578bb786
Lumian1711 <Lumian1711>
2024-10-21 22:33:52 -0600

    Inicio de sesion

---
+++ DataBase/scripts/sp_iniciar_sesion.sql
@@ -0,0 +1,32 @@
+DELIMITER 
+
+CREATE PROCEDURE iniciar_sesion (
+    IN p_nombre_usuario VARCHAR(50),
+    IN p_contrasena VARCHAR(50),
+    OUT p_rol ENUM('estudiante', 'instructor', 'admin'),
+    OUT p_estatus ENUM('activo', 'bloqueado', 'eliminado'),
+    OUT p_resultado INT
+)
+BEGIN
+    DECLARE db_contrasena VARCHAR(255);
+
+    SELECT contrasena, rol, estatus 
+    INTO db_contrasena, p_rol, p_estatus
+    FROM usuario 
+    WHERE nombre_usuario = p_nombre_usuario;
+    
+    IF db_contrasena IS NULL THEN
+        SET p_resultado = 0;
+    ELSE
+        IF p_estatus != 'activo' THEN
+            SET p_resultado = 2;        ELSE
+
+            IF p_contrasena = db_contrasena THEN
+                SET p_resultado = 1;             ELSE
+                SET p_resultado = 3;            END IF;
+        END IF;
+    END IF;
+
+END 
+
+DELIMITER ;
